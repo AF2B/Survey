2b2e990be3217d7dd2490b934721716bb6ce2202
andre.borba <dev_andre.filipe@outlook.com>
2023-06-11 01:35:03 -0300

    feat: use context provider

--- survey_react/src/main.tsx
+++ survey_react/src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AppRoutes from './routes/AppRoutes.tsx'
 import './index.css'
+import { ContextProvider } from './contexts/ContextProvider.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <AppRoutes />
+    <ContextProvider>
+        <AppRoutes />
+    </ContextProvider>
   </React.StrictMode>,
 )
