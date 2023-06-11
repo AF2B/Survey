00c07a0f2113efc1f80ce6212a635246ed36953a
andre.borba <dev_andre.filipe@outlook.com>
2023-06-11 01:35:31 -0300

    feat: changed component to use children

--- survey_react/src/views/Dashboard.tsx
+++ survey_react/src/views/Dashboard.tsx
@@ -1,20 +1,11 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import Page from '../components/Page';
 
 const Dashboard: React.FC = () => {
     return (
-        <div>
-            <header className="bg-white shadow">
-                <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                    <h1 className="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
-                </div>
-            </header>
-            <main>
-                <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-                    <Outlet />
-                </div>
-            </main>
-        </div>
+        <Page title='Dashboard'>
+            Dashboard Content
+        </Page>
     );
 }
 
